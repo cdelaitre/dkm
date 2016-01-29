bdbcee4943e53ddbb4b78fb0a8f9ab16b0274209
cdelaitre <christophe.delaitre@externes.sg.social.gouv.fr>
2016-01-29 15:33:52 +0100

    add multiple volumes

--- Makefile
+++ Makefile
@@ -1,19 +1,2 @@
-#!/bin/bash
-#include dkm.conf
-#include _dkm/*
-
-setup-check:
-	DKM=`pwd`/dkm
-	@if [[ -L $$DKM ]]; then\
-                echo "$$0 Setup INFO $$DKM OK";\
-        else\
-                echo "$$0 Setup ERR $$DKM KO";\
-        fi
-
-setup-link:
-	DKM=`pwd`/dkm
-	@if [ ! -L ${DKM} ]; then\
-		@ln -s ${DKM} /usr/local/bin/dkm;\
-	fi
-
-setup: setup-link setup-check
+include ${DKM_HOME}/dkm.conf
+include ${DKM_HOME}/_dkm/*
--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-function dkm-setup-check {
+function dkm-setup-check-file {
+        if [[ -x ${SOURCE} ]]; then
+                echo "[$0] INFO ${SOURCE} is an executable file OK";
+        else
+                echo "[$0] ERR ${SOURCE} is not an executabe file KO";
+        fi
+}
+
+function dkm-setup-check-link {
 	if [[ -L ${DEST} ]]; then
                 echo "[$0] INFO ${DEST} is a link OK";
         else
@@ -14,14 +22,22 @@ function dkm-setup-link {
 	fi
 }
 
-function dkm-setup {
-	dkm-setup-link
-	dkm-setup-check
+function dkm-setup-export-home {
+	export DKM_HOME=${DKM_HOME}
+	echo DKM_HOME=${DKM_HOME}
 }
 
-source dkm.conf 
+function dkm-setup {
+	dkm-setup-export-home
+	dkm-setup-check-file
+	dkm-setup-link
+	dkm-setup-check-link
+}
 
-SOURCE=`pwd`/bin/dkm
-DEST=${DKM_SETUP_PATH}/dkm
+source ./dkm.conf 
+
+SOURCE=${DKM_HOME}/bin/dkm
+DEST=${DKM_PATH}/dkm
 
 dkm-setup
+
